036b4c7d068b4b4501a520ce2b0b6165a32bae5f
skythoughtco-png <skythought.co@gmail.com>
2026-06-30 11:53:55 +0300

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,27 @@
+const topBtn = document.getElementById("topBtn");
+
+window.onscroll = function () {
+
+    if (window.scrollY > 300) {
+
+        topBtn.style.display = "block";
+
+    } else {
+
+        topBtn.style.display = "none";
+
+    }
+
+};
+
+topBtn.onclick = function () {
+
+    window.scrollTo({
+
+        top: 0,
+
+        behavior: "smooth"
+
+    });
+
+};
